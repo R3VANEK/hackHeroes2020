d5dd5a862151a9bdba6049a14280f164dc32bf2e
Jan Napieralski <janek2000-2000@wp.pl>
2020-10-13 23:39:01 +0200

    dalsze stylowania

--- src/components/User/LeftPanel.js
+++ src/components/User/LeftPanel.js
@@ -1,18 +1,59 @@
 import React from 'react'
 import './LeftPanel.css'
+import videoIcon from '../../icons/video-white.svg';
+import receiptIcon from '../../icons/receipt-white.svg';
+import graphIcon from '../../icons/graph-white.svg';
+import accountIcon from '../../icons/account-white.svg';
+import doctorIcon from '../../icons/doctor-white.svg';
+
+
 
 const LeftPanel = () =>{
+
+    
+
     return(
         <aside>
             <p class="aside-title-span">Zakładki</p>
 
-            <div class="aside-option-div">Teleporada</div>
-            <div class="aside-option-div">Twoje recepty</div>
-            <div class="aside-option-div">Twoje statystyki</div>
-            <div class="aside-option-div">Twoje konto</div>
-            <div class="aside-option-div">Dostępni lekarze</div>
+            <div class="aside-option-div">
+                <img src={videoIcon} class="option-img"/>Teleporada
+            </div>
+
+            
+            <div class="aside-option-div">
+                <img src={receiptIcon} class="option-img"/> Twoje recepty
+            </div>
+
+            <div class="aside-option-div">
+                <img src={graphIcon} class="option-img"/> Twoje statystyki
+            </div>
+
+            <div class="aside-option-div">
+                <img src={accountIcon} class="option-img"/> Twoje konto
+            </div>
+
+            <div class="aside-option-div">
+                <img src={doctorIcon} class="option-img"/> Dostępni lekarze
+            </div>
 
             <p class="aside-title-span">Ostatnie spotkania</p>
+             
+            <div class="aside-last-call-holder">
+                <div className="aside-doctor-picture-holder"></div>
+                <p className="aside-doctor-user-name">Dr James Watson</p>   
+            </div>
+
+            <div class="aside-last-call-holder">
+                <div className="aside-doctor-picture-holder"></div>
+                <p className="aside-doctor-user-name">Dr Johny Sins</p>   
+            </div>
+
+            <div class="aside-last-call-holder">
+                <div className="aside-doctor-picture-holder"></div>
+                <p className="aside-doctor-user-name">Dr Dree</p>   
+            </div>
+
 
         </aside>
     )
--- src/components/User/Navbar.js
+++ src/components/User/Navbar.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import './Navbar.css';
+import appIcon from '../../icons/healfy.svg.png';
+import profPicture from '../../images/sam.jpeg';
 
 class Navbar extends Component {
     
     render() { 
+
+        const profilePictureSetter = {
+            backgroundImage : `url(${profPicture})`
+        }
+
+
         return ( 
             <nav>
                 <div id="nav-left-holder">
-                    
+                    <img src={appIcon} id="healfyIconNav"/>
+                    healfy
                 </div>
+
+                <div className="option-link-holder">
+                    Strona główna
+                </div>
+
+                <div className="option-link-holder">
+                    Kontakt
+                </div>
+
+                <div className="option-link-holder">
+                    O nas
+                </div>
+
+                <div id="nav-account-holder">
+                    <div id="profile-picture-holder" style={profilePictureSetter}></div>
+                    <p id="nav-user-name">John Doe</p>   
+                </div>
+
             </nav>
          )
     }
