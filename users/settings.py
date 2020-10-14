42fbda37afc69fb92b7d07b9cb8f81b8cc42ad5b
stepkos <52202262+stepkos@users.noreply.github.com>
2020-10-14 19:28:00 +0200

    Instaling REST Framework

--- users/settings.py
+++ users/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # REST Framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
