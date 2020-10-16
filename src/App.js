a3dcead1c9101e13de116e0ce718dc7a52310b52
Jan Napieralski <janek2000-2000@wp.pl>
2020-10-16 15:32:15 +0200

    poprawiona struktura aplikacji, działające linki etc

--- src/App.js
+++ src/App.js
@@ -6,13 +6,24 @@ import './App.css';
 import Mainholder from './components/User/Mainholder';
 import RegisterForm from './components/Form/RegisterForm';
 
+import Teleporada from './components/User/Teleporada'
+import Recepty from './components/User/Recepty'
+import Statystyki from './components/User/Statystyki'
+import Konto from './components/User/Konto'
+import Lekarze from './components/User/Lekarze'
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         
         <Route path="/register" component={RegisterForm}/>
-        <Route path="/" component={Mainholder}/>
+
+        <Route path="/teleporada" component={Mainholder(Teleporada)}/>
+        <Route path="/recepty" component={Mainholder(Recepty)}/>
+        <Route path="/statystyki" component={Mainholder(Statystyki)}/>
+        <Route path="/konto" component={Mainholder(Konto)}/>
+        <Route path="/lekarze" component={Mainholder(Lekarze)}/>
         
       </Switch>
     </BrowserRouter>
