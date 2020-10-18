8207a0202f83b3b00002e9aab3335e72ff6653c0
Jan Napieralski <janek2000-2000@wp.pl>
2020-10-18 21:26:39 +0200

    działający form rejestracji, bez walidacji

--- src/components/Form/RegisterForm.js
+++ src/components/Form/RegisterForm.js
@@ -5,11 +5,61 @@ import appIcon from '../../icons/healfy.svg.png';
 class RegisterForm extends Component {
 
 
-    state = {  }
+    constructor(props){
+        super(props)
+        this.patientRef = React.createRef()
+        this.doctorRef = React.createRef()
+    }
+    componentDidMount(){
+        this.patientRef.current.style.backgroundColor = 'white'
+        this.patientRef.current.style.color = '#5772f0'
+    }
 
+    state = { 
+        role : 'Pacjent',
+        firstName : '',
+        lastName : '',
+        password : '',
+        checkPassword : '',
+        phoneNumber : '',
+        emailAddres : '',
+
+        errorMessage : ''
+     }
+
+     handleChange = (event) =>{
+         console.log(event.target.name)
+         this.setState({
+             [event.target.name] : event.target.value
+         })
+     }
+
+
+     changeRole = (id) =>{
+            if(id == 0){
+               
+                this.doctorRef.current.style.backgroundColor = '#5772f0'
+                this.doctorRef.current.style.color = 'white'
+                this.patientRef.current.style.backgroundColor = 'white'
+                this.patientRef.current.style.color = '#5772f0'
+                this.setState({
+                    role : 'Pacjent'
+                })
+            }
+            else{
+                this.patientRef.current.style.backgroundColor = '#5772f0'
+                this.patientRef.current.style.color = 'white'
+                this.doctorRef.current.style.backgroundColor = 'white'
+                this.doctorRef.current.style.color = '#5772f0'
+                this.setState({
+                    role : 'Doktor'
+                })
+            }
+    }
 
     render() { 
 
+        
 
         return ( 
             <div id="formPage">
@@ -31,9 +81,42 @@ class RegisterForm extends Component {
                 <section id="register-right-holder">
 
                     <div id="role-changer-holder">
-                        <div className="role-changer">Pacjent</div>
-                        <div className="role-changer">Doktor</div>
+                        <div className="role-changer" 
+                            ref={this.patientRef} 
+                            onClick={()=>{this.changeRole(0)}}>
+                            Pacjent
+                        </div>
+
+                        <div className="role-changer" 
+                            ref={this.doctorRef} 
+                            onClick={()=>{this.changeRole(1)}}>
+                            Doktor
+                        </div>
                     </div>
+
+                    
+                    <form>
+                
+                    <div id="help-holder">
+                        <p id="register-as">Zarejestruj się jako {this.state.role}</p>
+                    </div>
+                    <div class="section-inputs">
+                        <input type="text" placeholder="Imię" name="firstName" class="register-inputs" onChange={this.handleChange}/>
+                        <input type="text" placeholder="Nazwisko" name="lastName" class="register-inputs" onChange={this.handleChange}/>
+                        <input type="password" placeholder="Hasło" name="password"class="register-inputs" onChange={this.handleChange}/>
+                        <input type="password" placeholder="Powtórz hasło" name="checkPassword" class="register-inputs" onChange={this.handleChange}/>
+                    </div>
+                    <div class="section-inputs">
+                        <input type="text" placeholder="Telefon" name="phoneNumber" class="register-inputs" onChange={this.handleChange}/>
+                        <input type="email" placeholder="Email" name="emailAddres" class="register-inputs" onChange={this.handleChange}/>
+                        <input type="submit" value="Zarejestruj się" id="register-button"/>
+
+                        <p id="error-p">{this.state.errorMessage}</p>
+                    </div>
+
+                    </form>
+                    
+
 
                 </section>
             </div>
