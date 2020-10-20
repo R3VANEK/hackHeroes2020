3b83153003b59557ad92f96eee889a78b3961d51
Jan Napieralski <janek2000-2000@wp.pl>
2020-10-20 23:22:26 +0200

    działający div z wpisanymi statystykami

--- src/components/User/Statystyki/Statystyki.js
+++ src/components/User/Statystyki/Statystyki.js
@@ -15,21 +15,72 @@ import HearthIcon from '../../../icons/hearth.svg'
 
 
 class Statystyki extends Component {
-    state = { 
-        date: new Date(),
-        mood : 10,
-        temperature : 36.6,
-        sleep : 8,
-        pulse : 120
+
+    componentDidMount(){
+        document.getElementsByClassName('date-prev-holder')[5].classList.add('active')
     }
 
-    onChange = date => this.setState({ date })
+
+    state = { 
+        shortMonths : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        currentDate : new Date(Date.now()),
+    
+        lastDaysData : [
+            { date : new Date(Date.now()), mood : 10, temperature : 36.6, sleep: 8, pulse : 120},
+            { date : new Date(new Date().setDate(new Date().getDate()-1)), mood : 8, temperature : 37.0, sleep: 10, pulse : 110},
+            { date : new Date(new Date().setDate(new Date().getDate()-2)), mood : 6, temperature : 37.5, sleep: 6, pulse : 100},
+            { date : new Date(new Date().setDate(new Date().getDate()-3)), mood : 5, temperature : 38.0, sleep: 5, pulse : 110},
+            { date : new Date(new Date().setDate(new Date().getDate()-4)), mood : 6, temperature : 37.2, sleep: 8, pulse : 120},
+            { date : new Date(new Date().setDate(new Date().getDate()-5)), mood : 7, temperature : 36.9, sleep: 9, pulse : 90}
+        ],
+
+        currentDisplayData : {date : new Date(Date.now()), mood : 10, temperature : 36.6, sleep: 8, pulse : 120}
+    }
+
+    onCalendarChange = (date) => {
+        this.setState({ date })
+    }
 
     statsChange = (event, parameter) =>{
-        this.setState({[parameter] : event.target.value})
+        let date = this.state.currentDisplayData.date
+        let copyState = this.state.lastDaysData
+        let index = copyState.findIndex((el)=>{
+            console.log(el.date, date)
+            return el.date.getTime() == date.getTime()
+        })
+        
+        let element = copyState[index]
+        console.log(element)
+        element[parameter] = event.target.value
+        copyState[index] = element
+        let prevCurrent = this.state.currentDisplayData
+        this.setState({lastDaysData : [...copyState]})
+        this.setState({currentDisplayData : {
+                ...prevCurrent,
+                [parameter] : event.target.value
+            }
+        })
     }
 
+    changeDisplayData = (id) =>{
+        for(let i = 0; i<6;i++){
+            document.getElementsByClassName('date-prev-holder')[i].classList.remove('active')
+        }
+        
+        document.getElementsByClassName('date-prev-holder')[5-id].classList.add('active')
+
+
+        this.setState({
+            currentDisplayData : {
+                ...this.state.lastDaysData[id]
+            }
+        })
+    }
+
+
+
     render() { 
+
         return ( 
             <main>
                 <div id="lekarze-top-holder">
@@ -43,13 +94,14 @@ class Statystyki extends Component {
                 </div>
 
                 <div id="lekarze-left-holder">
+                        
                     <div id="date-help-holder">
-                        <div class="date-prev-holder">14 <br/>Sep</div>
-                        <div class="date-prev-holder">15 <br/>Sep</div>
-                        <div class="date-prev-holder">16 <br/>Sep</div>
-                        <div class="date-prev-holder">17 <br/>Sep</div>
-                        <div class="date-prev-holder">18 <br/>Sep</div>
-                        <div class="date-prev-holder">19 <br/>Sep</div>
+                        <div class="date-prev-holder" onClick={()=>{this.changeDisplayData(5)}}>{this.state.currentDate.getDate()-5}<br/>{this.state.shortMonths[this.state.currentDate.getMonth()]}</div>
+                        <div class="date-prev-holder" onClick={()=>{this.changeDisplayData(4)}}>{this.state.currentDate.getDate()-4}<br/>{this.state.shortMonths[this.state.currentDate.getMonth()]}</div>
+                        <div class="date-prev-holder" onClick={()=>{this.changeDisplayData(3)}}>{this.state.currentDate.getDate()-3}<br/>{this.state.shortMonths[this.state.currentDate.getMonth()]}</div>
+                        <div class="date-prev-holder" onClick={()=>{this.changeDisplayData(2)}}>{this.state.currentDate.getDate()-2}<br/>{this.state.shortMonths[this.state.currentDate.getMonth()]}</div>
+                        <div class="date-prev-holder" onClick={()=>{this.changeDisplayData(1)}}>{this.state.currentDate.getDate()-1}<br/>{this.state.shortMonths[this.state.currentDate.getMonth()]}</div>
+                        <div class="date-prev-holder" onClick={()=>{this.changeDisplayData(0)}}>{this.state.currentDate.getDate()}<br/>{this.state.shortMonths[this.state.currentDate.getMonth()]}</div>
                     </div>
                     
                     <div class="stats-holder">
@@ -57,7 +109,7 @@ class Statystyki extends Component {
                             <img src={MoodIcon} class="stats-icon"/>
                         </div>
                         <div class="stats-name-holder">
-                            Samopoczucie<span class="stats-colored lime text"> {this.state.mood}/10</span>
+                            Samopoczucie<span class="stats-colored lime text"> {this.state.currentDisplayData.mood}/10</span>
                         </div>
                         <input type="range" class="input-range" min="1" max="10" step="1" onChange={(event)=>{this.statsChange(event, 'mood')}}/>
                     </div>
@@ -67,7 +119,7 @@ class Statystyki extends Component {
                             <img src={TemperatureIcon} class="stats-icon "/>
                         </div>
                         <div class="stats-name-holder">
-                            Temperatura<span class="stats-colored red text"> {this.state.temperature} &deg; C</span>
+                            Temperatura<span class="stats-colored red text"> {this.state.currentDisplayData.temperature} &deg; C</span>
                         </div>
                         <input type="range" class="input-range" min="34.8" max="40" step="0.1"  onChange={(event)=>{this.statsChange(event, 'temperature')}}/>
                     </div>
@@ -77,7 +129,7 @@ class Statystyki extends Component {
                             <img src={MoonIcon} class="stats-icon "/>
                         </div>
                         <div class="stats-name-holder">
-                            Sen<span class="stats-colored purple text"> {this.state.sleep} godziny</span>
+                            Sen<span class="stats-colored purple text"> {this.state.currentDisplayData.sleep} godziny</span>
                         </div>
                         <input type="range" class="input-range" min="1" max="16" step="1" onChange={(event)=>{this.statsChange(event, 'sleep')}}/>
                     </div>
@@ -87,7 +139,7 @@ class Statystyki extends Component {
                             <img src={HearthIcon} class="stats-icon "/>
                         </div>
                         <div class="stats-name-holder">
-                            Puls<span class="stats-colored blue text"> {this.state.pulse} /min</span>
+                            Puls<span class="stats-colored blue text"> {this.state.currentDisplayData.pulse} /min</span>
                         </div>
                         <input type="range" class="input-range" min="40" max="200" step="10" onChange={(event)=>{this.statsChange(event, 'pulse')}}/>
                     </div>
@@ -96,7 +148,7 @@ class Statystyki extends Component {
 
                 <div id="lekarze-right-holder">
                     <Calendar
-                        onChange={this.onChange}
+                        onChange={this.onCalendarChange}
                         value={this.state.date}
                         className="calendar"
                     />
